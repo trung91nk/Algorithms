d42c8096881d09460a7289c1104b754ec3a6e7ef
trung91nk <41450923+trung91nk@users.noreply.github.com>
2020-04-07 17:46:33 +0200

    Update countingElements.java

--- LeetCode/30dayschallenge/countingElements.java
+++ LeetCode/30dayschallenge/countingElements.java
@@ -8,6 +8,7 @@ public class countingElements{
 		//int [] arr = {8,7,12,13,0,6,8,10,0,0,0,6,0,14};
 		System.out.println(count(arr));
 	}
+	//normalAlgo
 	public static int count(int[] arr){
 		int counted = 0;
 		Arrays.sort(arr);
@@ -22,6 +23,21 @@ public class countingElements{
 			}else{
 				pointer1=pointer2;
 				pointer2++;
+			}
+		}
+		return counted;
+	}
+	//HashSet
+	public static int count1(int[] arr){
+		Arrays.sort(arr);
+		HashSet<Integer> h = new HashSet<Integer>();
+		int counted =0;
+		for(int i=arr.length-1;i>=0;i--){
+			if(!h.contains(arr[i])){
+				h.add(arr[i]);
+			}
+			if(h.contains(arr[i]+1)){
+				counted++;
 			}
 		}
 		return counted;
