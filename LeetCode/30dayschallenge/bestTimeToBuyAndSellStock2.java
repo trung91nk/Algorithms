6dea0a3c6cb5d4dcd516a4d905d7a2d2bc6f5388
trung91nk <41450923+trung91nk@users.noreply.github.com>
2020-04-16 21:10:10 +0200

    Update bestTimeToBuyAndSellStock2.java

--- LeetCode/30dayschallenge/bestTimeToBuyAndSellStock2.java
+++ LeetCode/30dayschallenge/bestTimeToBuyAndSellStock2.java
@@ -1,74 +1,28 @@
 public class bestTimeToBuyAndSellStock2{
 	public static void main (String[] args){
 	}
-	public static int maxProfit(int[] prices){
-		int profit =0;
-		int l = prices.length;
-		if(l>=1){
-			int iMin = nSmall(prices);
-			int iMax = nBig(prices);
-			if(iMin==0 && iMax==l-1){     
-				profit +=prices[iMax]-prices[iMin];
-			}else if(iMin==l-1 && iMax==0){
-				profit = 0;
-			}else{
-				profit+=prices[iMax]-prices[iMin];
-				while(iMin<l-1 && iMax<l-1){
-					if(iMax<=l-2){
-						iMin=iMax+1;
-						if(iMin<=l-2){
-							while(prices[iMin]>=prices[iMin+1]){
-								iMin++;
-								if(iMin==l-1){
-									break;
-								}
-							}
-						}
+	public static int maxProfit(int[] prices){	
+        	int profit = 0;
+		int ptr = 0;
+		while(ptr<prices.length-1){
+			if(prices[ptr]<prices[ptr+1]){
+				if(ptr==prices.length-1){
+					profit= 0;
+				}else if(ptr==prices.length-2){
+					profit+= prices[ptr+1]-prices[ptr];
+					ptr++;
+				}else{
+					int iMax= ptr+1;
+					while(iMax<prices.length-1 && prices[iMax]<prices[iMax+1] ){
+						iMax++;
 					}
-					if(iMin<=l-2){
-						iMax=iMin+1;
-						if(iMax<=l-2){
-							while(prices[iMax]<=prices[iMax+1] && iMax<=l-2){
-								iMax++;
-								if(iMax==l-1){
-									break;
-								}
-							}
-						}
-						profit+=prices[iMax]-prices[iMin];
-					}
+					profit+=prices[iMax]-prices[ptr];
+					ptr = iMax+1;
 				}
+			}else{
+				ptr++;
 			}
-		}else{
-			profit =0;
 		}
 		return profit;
-	}
-	public static int nSmall(int [] n){
-		int i = 0;
-		while(i<n.length-1){
-			if(n[i]>n[i+1]){
-				i++;
-			}else{
-				break;
-			}
-		}
-		return i;
-	}
-	public static int nBig (int [] n){
-		int i = nSmall(n);
-		if(i>=n.length-1){
-			return 0;
-		}else{
-			i+=+1;
-			while(i<n.length-1){
-				if(n[i]<n[i+1]){
-					i++;
-				}else{
-					break;
-				}
-			}
-			return i;
-		}
 	}
 }
